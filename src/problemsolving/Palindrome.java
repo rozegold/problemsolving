0b21000d6092478b70998dae731b50ea006c25b9
Shreya Mishra <shreya@Shreyas-MacBook-Pro.local>
2017-09-10 11:51:16 +0200

    palindrome

--- src/problemsolving/Palindrome.java
+++ src/problemsolving/Palindrome.java
@@ -1,5 +1,21 @@
-package main;
+package problemsolving;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
 
 public class Palindrome {
 
+
+    boolean checkIfStringIsPalindrome(String str) {
+        if (null == str || str.isEmpty())
+            return false;
+
+        int length = str.length();
+        return !(IntStream.range(0, length / 2).anyMatch(i ->
+                str.charAt(i) != str.charAt(length - i - 1)
+        ));
+    }
 }
--- test/problemsolving/PalindromeTest.java
+++ test/problemsolving/PalindromeTest.java
@@ -1,5 +1,34 @@
+package problemsolving;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PalindromeTest {
 
+    private Palindrome palindrome;
+
+    @Before
+    public void setup(){
+        palindrome = new Palindrome();
+    }
+
+
+    private String validString = "abcba";
+    private String invalidString = "abcca";
+
+    @Test
+    public void testIfGivenStringIsPalindrome(){
+
+        assertEquals(true, palindrome.checkIfStringIsPalindrome(validString));
+        assertEquals(false, palindrome.checkIfStringIsPalindrome(invalidString));
+
+    }
+
+    @Test
+    public void testIFGivenStringIsNullOrEmpty(){
+        assertEquals(false,palindrome.checkIfStringIsPalindrome(""));
+        assertEquals(false,palindrome.checkIfStringIsPalindrome(null));
+    }
 }
